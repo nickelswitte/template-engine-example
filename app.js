7d833c02e7dbde6272dd42083310c7e2633283bf
nickelswitte <witte.nickels-tim17@it.dhbw-ravensburg.de>
2021-07-26 22:56:30 +0200

    Added loop to templates

--- app.js
+++ app.js
@@ -22,7 +22,51 @@ app.use(express.static('node_modules/bulma/css'));
 
 // Serve the index page
 app.get('/', (req, res) => {
-    res.render("index")
+    res.render("index", {
+        title: "This is the title",
+        subtitle: "This is the subtitle"
+    })
+})
+
+let data = {
+    title: "Find here some images",
+    subtitle: "They are just for you",
+    people: [
+        {
+            name: "John Taylor",
+            twitter: "jtee03",
+            description: "Wafer caramels danish bonbon. Cheesecake cookie sugar plum. I love ice cream pastry.",
+            imagelink: "https://i.imgur.com/2F3Al82.jpeg"
+        },
+        {
+            name: "Doreen Johnson",
+            twitter: "dorydance-xt",
+            description: "Cotton candy biscuit tootsie roll chupa chups I love jujubes lollipop. Dessert cheesecake I love I love tart chocolate cake brownie",
+            imagelink: "https://i.imgur.com/aUhpEz3.jpeg"
+        },
+        {
+            name: "Freerk Jennsen",
+            twitter: "freerk",
+            description: "Lorem Ipsum Bla Blop",
+            imagelink: "https://i.imgur.com/QdOTqIR.jpeg"
+        },
+        {
+            name: "Milli Gerogia",
+            twitter: "septemberagain",
+            description: "Cupcake ipsum dolor sit amet tiramisu. Tiramisu tiramisu muffin. Liquorice chocolate cake bear claw sweet apple pie I love halvah tart. Toffee marshmallow croissant powder. Wafer caramels danish bonbon. Cheesecake cookie sugar plum. I love ice cream pastry. Cotton candy biscuit tootsie roll chupa chups I love jujubes lollipop. Dessert cheesecake I love I love tart chocolate cake brownie.",
+            imagelink: "https://i.imgur.com/sBM9y7a.png"
+        },
+        {
+            name: "Lülli Widde",
+            twitter: "vanilly",
+            description: "Cupcake ipsum dolor sit amet tiramisu. Tiramisu tiramisu muffin. Liquorice chocolate cake bear claw sweet apple pie I love halvah tart. Toffee marshmallow croissant powder. Wafer caramels danish bonbon. Cheesecake cookie sugar plum.",
+            imagelink: "https://i.imgur.com/iEMP5I6.jpeg"
+        },
+    ]
+}
+// Serve the index page
+app.get('/i', (req, res) => {
+    res.render("images", data)
 })
 
 
